9223c6194fa8843bdeb85b89e15f25872b65ccec
JunMizuno <>
2019-09-09 16:58:29 +0900

    カメラの解像度のコントロール設定を追加

---
+++ unity_validation/Assets/Scripts/Camera/ControllRisizeBuffers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// @memo. CameraのAllow Dynamic Resolutionを有効にする
+/// @memo. RenderTextureのDynamic Scallingを有効にする
+/// @memo. PlayerSettings -> Other Settings -> Enable Frame Timingを有効にする
+/// </summary>
+public class ControllRisizeBuffers : MonoBehaviour
+{
+    private void Start()
+    {
+        // @memo. この引数値で解像度の度合いを設定
+        ScalableBufferManager.ResizeBuffers(0.75f, 0.75f);
+    }
+}
